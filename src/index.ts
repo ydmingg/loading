20841f4b2af744684a3b6c16f861a4070de6f86d
ydmingg <xm95zz@gmail.com>
2023-09-21 21:43:36 +0800

    updata base

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {loading} from "@src/tpl";
+
+let oApp = document.querySelector("#app") as HTMLElement;
+oApp.style.backgroundColor = "#dcdcd4"
+
+// 渲染
+oApp.innerHTML = loading.model3d
---
+++ src/tpl.ts
@@ -0,0 +1,3 @@
+export const loading={ 
+    model3d: `<div class="loading-spinner row rowcolumn align-center justify-content-center"><div class="loading-spinner-content"><div></div><div></div><div></div><div></div><div></div><div></div></div><div class="loading-spinner-text">页面加载中，请稍候</div></div>`, 
+}
